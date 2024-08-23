199378c737b12a211247cc19a862e21f172028a1
SohamMangale <sohammangale60@gmail.com>
2024-08-23 10:10:14 +0530

    Create Exp.6D.cpp

---
+++ Exp.6D.cpp
@@ -0,0 +1,15 @@
+//Soham
+//23070123084
+//entc B1
+//experiment 6D
+#include<iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 1; i++) {
+        for (int j = 1; j <= 10; j++) {
+            std::cout << j << " ";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
