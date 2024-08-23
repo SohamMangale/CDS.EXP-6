03294d065997d9543d54c01f2043ee8eeba3853d
SohamMangale <sohammangale60@gmail.com>
2024-08-23 09:53:32 +0530

    Create Exp-6.cpp

---
+++ Exp-6.cpp
@@ -0,0 +1,14 @@
+//Soham
+//23070123084
+//entc B1
+//experiment 6
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        cout<<"Hello world("<<i+1<<")"<<endl;
+    }
+}
