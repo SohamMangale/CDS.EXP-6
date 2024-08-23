db3be2d506a08583d900bb6223c765ae9c950937
SohamMangale <sohammangale60@gmail.com>
2024-08-23 09:55:42 +0530

    Create Exp-6B.cpp

---
+++ Exp-6B.cpp
@@ -0,0 +1,14 @@
+//Soham
+//23070123084
+//entc B1
+//experiment 6
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int input = 1;
+    while (input <= 15) {
+        std::cout << "\n" << input;
+        input++;
+    }
+}
