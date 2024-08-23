f61942a198e2c0a73a6198aae7b2ffaad499afb5
SohamMangale <sohammangale60@gmail.com>
2024-08-23 10:09:03 +0530

    Create Exp.6C.cpp

---
+++ Exp.6C.cpp
@@ -0,0 +1,18 @@
+//Soham
+//23070123084
+//entc B1
+//experiment 6C
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number = 1;
+    
+    do {
+        cout << number << endl;
+        number++;
+    } while (number <= 10);
+
+    return 0;
+}
